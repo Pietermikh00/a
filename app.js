a6e6321eac5e3fb2467a436eeba1eb845f316406
Pietermikh00 <163128406+Pietermikh00@users.noreply.github.com>
2025-03-24 14:51:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // 1) Layani file statis dari folder "public"
 app.use(express.static(path.join(__dirname, 'public')));
